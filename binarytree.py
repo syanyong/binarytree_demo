7b5eb524aacbc6c3d7b028832ab200affad40221
Sarucha Yanyong <sarucha.yanyong@th.transportation.bombardier.com>
2015-07-09 11:34:12 +0700

    Fix binary class and test.

--- binarytree.py
+++ binarytree.py
@@ -1,13 +1,13 @@
 # simple binary tree
 # in this implementation, a node is inserted between an existing node and the root
-
+import sys
 
 class BinaryTree():
 
     def __init__(self,rootid):
       self.left = None
       self.right = None
-      self.rootid = rootid
+      self.rootid = rootid  #Value
 
     def getLeftChild(self):
         return self.left
@@ -19,6 +19,7 @@ class BinaryTree():
         return self.rootid
 
     def insertRight(self,newNode):
+        print(self.right)
         if self.right == None:
             self.right = BinaryTree(newNode)
         else:
@@ -27,12 +28,13 @@ class BinaryTree():
             self.right = tree
         
     def insertLeft(self,newNode):
+        print(self.left)
         if self.left == None:
             self.left = BinaryTree(newNode)
         else:
             tree = BinaryTree(newNode)
-            self.left = tree
             tree.left = self.left
+            self.left = tree
 
 
 def printTree(tree):
@@ -40,6 +42,7 @@ def printTree(tree):
             printTree(tree.getLeftChild())
             print(tree.getNodeValue())
             printTree(tree.getRightChild())
+            
             
 
 
@@ -51,4 +54,22 @@ def testTree():
     myTree.insertRight("Tony")
     myTree.insertRight("Steven")
     printTree(myTree)
-        
+
+## Binary Tree
+testTree()
+
+t = BinaryTree("Start")
+t.insertLeft("LEFT1")
+t.insertLeft("LEFT2")
+t.insertLeft("LEFT3")
+t.insertRight("LEFTs")
+
+print("TEST")
+while(True):
+    if (t != None):
+        print(t.getNodeValue())
+        t = t.getLeftChild()
+        print("OK")
+    else:
+        print("BREAK")
+        break
